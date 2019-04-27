7d1f6ac1b06de426753968dd7cc19cb11c3e02d6
Florent Thoumie <florent@thoumie.net>
2019-04-27 11:52:08 -0700

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ test_requirements = ["asynctest", "pytest", "pytest-cov", "pytest-asyncio"]
 
 setup(
     name="iaqualink",
-    version="0.1.0",
+    version="0.1.1",
     description="Asynchronous library for Jandy iAqualink",
     long_description=readme,
     long_description_content_type="text/markdown",
--- src/iaqualink/__init__.py
+++ src/iaqualink/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "Florent Thoumie"
 __email__ = "florent@thoumie.net"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from .client import AqualinkClient
 from .system import AqualinkSystem
