478ef6419d3a1c1379d571eab973b0f6d2bada41
Florent Thoumie <florent@thoumie.net>
2019-05-03 18:12:08 -0700

    Create new const.py.

--- src/iaqualink/client.py
+++ src/iaqualink/client.py
@@ -4,15 +4,15 @@ from typing import Dict, Optional
 
 import aiohttp
 
+from iaqualink.const import (
+    AQUALINK_API_KEY,
+    AQUALINK_LOGIN_URL,
+    AQUALINK_DEVICES_URL,
+    AQUALINK_SESSION_URL,
+)
 from iaqualink.system import AqualinkSystem
 from iaqualink.typing import Payload
 
-
-AQUALINK_API_KEY = "EOOEMOW4YR6QNB07"
-
-AQUALINK_LOGIN_URL = "https://support.iaqualink.com/users/sign_in.json"
-AQUALINK_DEVICES_URL = "https://support.iaqualink.com/devices.json"
-AQUALINK_SESSION_URL = "https://iaqualink-api.realtime.io/v1/mobile/session.json"
 
 AQUALINK_COMMAND_GET_DEVICES = "get_devices"
 AQUALINK_COMMAND_GET_HOME = "get_home"
---
+++ src/iaqualink/const.py
@@ -0,0 +1,5 @@
+AQUALINK_API_KEY = "EOOEMOW4YR6QNB07"
+
+AQUALINK_LOGIN_URL = "https://support.iaqualink.com/users/sign_in.json"
+AQUALINK_DEVICES_URL = "https://support.iaqualink.com/devices.json"
+AQUALINK_SESSION_URL = "https://iaqualink-api.realtime.io/v1/mobile/session.json"
