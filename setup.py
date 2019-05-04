1fea5502fa3b78ba5c19bc3d3780dd6ac07ea261
Florent Thoumie <florent@thoumie.net>
2019-05-03 21:10:30 -0700

    Bump version: 0.1.2 → 0.2.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ test_requirements = ["asynctest", "pytest", "pytest-cov", "pytest-asyncio"]
 
 setup(
     name="iaqualink",
-    version="0.1.2",
+    version="0.2.0",
     description="Asynchronous library for Jandy iAqualink",
     long_description=readme,
     long_description_content_type="text/markdown",
