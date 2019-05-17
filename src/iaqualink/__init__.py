05a044c73a84bb1f19f1e3846892004ab6e582d0
Florent Thoumie <florent@thoumie.net>
2019-05-16 18:14:40 -0700

    Bump version: 0.2.2 → 0.2.3

--- src/iaqualink/__init__.py
+++ src/iaqualink/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "Florent Thoumie"
 __email__ = "florent@thoumie.net"
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 from .client import AqualinkClient
 from .system import AqualinkSystem, AqualinkPoolSystem
