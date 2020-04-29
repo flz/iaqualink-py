95e7fddc60d524a52e2ce27d473fab264008dc3e
Florent Thoumie <florent@thoumie.net>
2020-04-28 19:42:43 -0700

    Bump version: 0.3.1 → 0.3.2

--- src/iaqualink/__init__.py
+++ src/iaqualink/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 __author__ = "Florent Thoumie"
 __email__ = "florent@thoumie.net"
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 
 from .client import AqualinkClient, AqualinkLoginException
