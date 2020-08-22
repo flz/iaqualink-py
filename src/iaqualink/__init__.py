681bcdcd6cbf31fe9dfd359c1f0a028c3817f7bd
Steve Onorato <open.source@steveonorato.com>
2020-08-21 18:19:52 -0700

    Bump version: 0.3.4 → 0.3.5

--- src/iaqualink/__init__.py
+++ src/iaqualink/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 __author__ = "Florent Thoumie"
 __email__ = "florent@thoumie.net"
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 
 
 from .client import AqualinkClient, AqualinkLoginException
