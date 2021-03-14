d7fe500db238eb63adedb172eb51c2e8581aa914
Florent Thoumie <florent@thoumie.net>
2021-03-14 13:31:24 -0700

    [const] Update API endpoint URL

--- src/iaqualink/const.py
+++ src/iaqualink/const.py
@@ -6,7 +6,7 @@ AQUALINK_API_KEY = "EOOEMOW4YR6QNB07"
 AQUALINK_LOGIN_URL = "https://support.iaqualink.com/users/sign_in.json"
 AQUALINK_DEVICES_URL = "https://support.iaqualink.com/devices.json"
 AQUALINK_SESSION_URL = (
-    "https://iaqualink-api.realtime.io/v1/mobile/session.json"
+    "https://iaqualink-api.iaqualink.net/v1/mobile/session.json"
 )
 
 AQUALINK_TEMP_CELSIUS_LOW = 1
