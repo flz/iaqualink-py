a2e7f6e1d380a67e9b8d0f0a99b19323e14585d4
Florent Thoumie <florent@thoumie.net>
2022-04-25 14:41:48 -0700

    Update tests to use new classes

--- tests/systems/exo/test_device.py
+++ tests/systems/exo/test_device.py
@@ -1,249 +1,314 @@
 from __future__ import annotations
 
 import copy
-import unittest
-from unittest.mock import AsyncMock, MagicMock
+from typing import cast
 
 import pytest
 
 from iaqualink.systems.exo.device import (
     ExoAttributeSensor,
-    ExoAttributeToggle,
-    ExoAuxToggle,
+    ExoAttributeSwitch,
+    ExoAuxSwitch,
     ExoDevice,
     ExoSensor,
     ExoThermostat,
-    ExoToggle,
+    ExoSwitch,
+    EXO_TEMP_CELSIUS_HIGH,
+    EXO_TEMP_CELSIUS_LOW,
+)
+from iaqualink.systems.exo.system import ExoSystem
+
+from ...base_test_device import (
+    TestBaseDevice,
+    TestBaseSensor,
+    TestBaseSwitch,
+    TestBaseThermostat,
 )
 
-from ...common import async_noop
 
-
-class TestExoDevice(unittest.IsolatedAsyncioTestCase):
+class TestExoDevice(TestBaseDevice):
     def setUp(self) -> None:
-        system = MagicMock()
-        data = {"name": "Test Device"}
-        self.obj = ExoDevice(system, data)
+        super().setUp()
+
+        data = {"serial_number": "SN123456", "device_type": "exo"}
+        self.system = ExoSystem(self.client, data=data)
+
+        data = {"name": "Test Device", "state": "42"}
+        self.sut = ExoDevice(self.system, data)
+        self.sut_class = ExoDevice
 
     def test_equal(self) -> None:
-        assert self.obj == self.obj
+        assert self.sut == self.sut
 
     def test_not_equal(self) -> None:
-        obj2 = copy.deepcopy(self.obj)
+        obj2 = copy.deepcopy(self.sut)
         obj2.data["name"] = "Test Device 2"
-        assert self.obj != obj2
+        assert self.sut != obj2
+
+    def test_property_name(self) -> None:
+        assert self.sut.name == self.sut.data["name"]
+
+    def test_property_state(self) -> None:
+        assert self.sut.state == str(self.sut.data["state"])
 
     def test_not_equal_different_type(self) -> None:
-        assert (self.obj == {}) is False
+        assert (self.sut == {}) is False
+
+    def test_property_manufacturer(self) -> None:
+        assert self.sut.manufacturer == "Zodiac"
+
+    def test_property_model(self) -> None:
+        assert self.sut.model == self.sut_class.__name__.replace("Exo", "")
 
 
-class TestExoSensor(unittest.IsolatedAsyncioTestCase):
+class TestExoSensor(TestExoDevice, TestBaseSensor):
     def setUp(self) -> None:
-        self.system = system = MagicMock()
+        super().setUp()
+
         data = {
             "name": "sns_1",
             "sensor_type": "Foo",
             "value": 42,
             "state": 1,
         }
-        self.obj = ExoDevice.from_data(system, data)
+        self.sut = ExoDevice.from_data(self.system, data)
+        self.sut_class = ExoSensor
 
-    def test_from_data(self) -> None:
-        assert isinstance(self.obj, ExoSensor)
+    def test_property_name(self) -> None:
+        assert self.sut.name == self.sut.data["sensor_type"].lower().replace(
+            " ", "_"
+        )
 
-    def test_attributes(self) -> None:
-        assert self.obj.name == "foo"
-        assert self.obj.label == "Foo"
-        assert self.obj.manufacturer == "Zodiac"
-        assert self.obj.model == "Sensor"
-        assert self.obj.state == "42"
+    def test_property_state(self) -> None:
+        assert self.sut.state == str(self.sut.data["value"])
 
-    def test_state_unavailable(self) -> None:
-        self.obj.data["state"] = 0
-        assert self.obj.state == ""
+    # def test_state_unavailable(self) -> None:
+    #    self.sut.data["state"] = 0
+    #    assert self.sut.state == ""
 
 
-class TestExoAttributeSensor(unittest.IsolatedAsyncioTestCase):
+class TestExoAttributeSensor(TestExoDevice):
     def setUp(self) -> None:
-        self.system = system = MagicMock()
+        super().setUp()
+
         data = {
             "name": "foo_bar",
             "state": 42,
         }
-        self.obj = ExoDevice.from_data(system, data)
-
-    def test_from_data(self) -> None:
-        assert isinstance(self.obj, ExoAttributeSensor)
-
-    def test_attributes(self) -> None:
-        assert self.obj.name == "foo_bar"
-        assert self.obj.label == "Foo Bar"
-        assert self.obj.manufacturer == "Zodiac"
-        assert self.obj.model == "AttributeSensor"
-        assert self.obj.state == "42"
+        self.sut = ExoDevice.from_data(self.system, data)
+        self.sut_class = ExoAttributeSensor
 
 
-class TestExoToggle(unittest.IsolatedAsyncioTestCase):
+class ExoSwitchMixin:
+    def test_property_is_on_false(self) -> None:
+        self.sut.data["state"] = 0
+        super().test_property_is_on_false()
+        assert self.sut.is_on is False
+
+    def test_property_is_on_true(self) -> None:
+        self.sut.data["state"] = 1
+        super().test_property_is_on_true()
+        assert self.sut.is_on is True
+
+
+class TestExoSwitch(TestExoDevice, ExoSwitchMixin, TestBaseSwitch):
     def setUp(self) -> None:
-        self.system = system = MagicMock()
+        super().setUp()
+
         data = {
             "name": "toggle",
             "state": 0,
         }
-        self.obj = ExoToggle(system, data)
 
-    async def test_toggle_not_implemented(self):
+        # ExoSwitch is an abstract class, not meant to be instantiated directly.
+        self.sut = ExoSwitch(self.system, data)
+        self.sut_class = ExoSwitch
+
+    async def test_turn_on(self) -> None:
+        self.sut.data["state"] = 0
         with pytest.raises(NotImplementedError):
-            await self.obj.toggle()
+            await super().test_turn_on()
+
+    async def test_turn_on_noop(self) -> None:
+        self.sut.data["state"] = 1
+        await super().test_turn_on_noop()
+
+    async def test_turn_off(self) -> None:
+        self.sut.data["state"] = 1
+        with pytest.raises(NotImplementedError):
+            await super().test_turn_off()
+
+    async def test_turn_off_noop(self) -> None:
+        self.sut.data["state"] = 0
+        await super().test_turn_off_noop()
 
 
-class TestExoAuxToggle(unittest.IsolatedAsyncioTestCase):
+class TestExoAuxSwitch(TestExoDevice, ExoSwitchMixin, TestBaseSwitch):
     def setUp(self) -> None:
-        self.system = system = MagicMock()
-        system.set_aux = async_noop
+        super().setUp()
+
         data = {
             "name": "aux_1",
             "type": "Foo",
             "mode": "mode",
             "light": 0,
-            "state": 0,
+            "state": 1,
         }
-        self.obj = ExoDevice.from_data(system, data)
-
-    def test_from_data(self) -> None:
-        assert isinstance(self.obj, ExoAuxToggle)
-
-    def test_attributes(self) -> None:
-        assert self.obj.name == "aux_1"
-        assert self.obj.label == "Aux 1"
-        assert self.obj.manufacturer == "Zodiac"
-        assert self.obj.model == "AuxToggle"
-        assert self.obj.state == "0"
+        self.sut = ExoDevice.from_data(self.system, data)
+        self.sut_class = ExoAuxSwitch
 
     async def test_turn_on(self) -> None:
-        self.system.set_aux.reset_mock()
-        self.obj.data["state"] = 0
-        await self.obj.turn_on()
-        self.system.set_aux.assert_called_with("aux_1", 1)
+        self.sut.data["state"] = 0
+        await super().test_turn_on()
 
     async def test_turn_on_noop(self) -> None:
-        self.system.set_aux.reset_mock()
-        self.obj.data["state"] = 1
-        await self.obj.turn_on()
-        self.system.set_aux.assert_not_called()
+        self.sut.data["state"] = 1
+        await super().test_turn_on_noop()
 
     async def test_turn_off(self) -> None:
-        self.system.set_aux.reset_mock()
-        self.obj.data["state"] = 1
-        await self.obj.turn_off()
-        self.system.set_aux.assert_called_with("aux_1", 0)
+        self.sut.data["state"] = 1
+        await super().test_turn_off()
 
     async def test_turn_off_noop(self) -> None:
-        self.system.set_aux.reset_mock()
-        self.obj.data["state"] = 0
-        await self.obj.turn_off()
-        self.system.set_aux.assert_not_called()
+        self.sut.data["state"] = 0
+        await super().test_turn_off_noop()
 
 
-class TestExoAttributeToggle(unittest.IsolatedAsyncioTestCase):
+class TestExoAttributeSwitch(TestExoDevice, ExoSwitchMixin, TestBaseSwitch):
     def setUp(self) -> None:
-        self.system = system = MagicMock()
-        system.set_toggle = async_noop
+        super().setUp()
+
         data = {
             "name": "boost",
-            "state": 0,
+            "state": 1,
         }
-        self.obj = ExoDevice.from_data(system, data)
-
-    def test_from_data(self) -> None:
-        assert isinstance(self.obj, ExoAttributeToggle)
-
-    def test_attributes(self):
-        assert self.obj.name == "boost"
-        assert self.obj.label == "Boost"
-        assert self.obj.manufacturer == "Zodiac"
-        assert self.obj.model == "AttributeToggle"
-        assert self.obj.state == "0"
+        self.sut = ExoDevice.from_data(self.system, data)
+        self.sut_class = ExoAttributeSwitch
 
     async def test_turn_on(self) -> None:
-        self.system.set_toggle.reset_mock()
-        self.obj.data["state"] = 0
-        await self.obj.turn_on()
-        self.system.set_toggle.assert_called_with("boost", 1)
+        self.sut.data["state"] = 0
+        await super().test_turn_on()
 
     async def test_turn_on_noop(self) -> None:
-        self.system.set_toggle.reset_mock()
-        self.obj.data["state"] = 1
-        await self.obj.turn_on()
-        self.system.set_toggle.assert_not_called()
+        self.sut.data["state"] = 1
+        await super().test_turn_on_noop()
 
     async def test_turn_off(self) -> None:
-        self.system.set_toggle.reset_mock()
-        self.obj.data["state"] = 1
-        await self.obj.turn_off()
-        self.system.set_toggle.assert_called_with("boost", 0)
+        self.sut.data["state"] = 1
+        await super().test_turn_off()
 
     async def test_turn_off_noop(self) -> None:
-        self.system.set_toggle.reset_mock()
-        self.obj.data["state"] = 0
-        await self.obj.turn_off()
-        self.system.set_toggle.assert_not_called()
+        self.sut.data["state"] = 0
+        await super().test_turn_off_noop()
 
 
-class TestExoThermostat(unittest.IsolatedAsyncioTestCase):
+class TestExoThermostat(TestExoDevice, TestBaseThermostat):
     def setUp(self) -> None:
-        self.system = system = AsyncMock()
-        data = {
+        super().setUp()
+
+        pool_set_point = {
             "name": "heating",
             "enabled": 1,
             "sp": 20,
             "sp_min": 1,
             "sp_max": 40,
         }
-        self.obj = ExoDevice.from_data(system, data)
 
-    def test_from_data(self) -> None:
-        assert isinstance(self.obj, ExoThermostat)
+        self.pool_set_point = cast(
+            ExoThermostat, ExoDevice.from_data(self.system, pool_set_point)
+        )
 
-    def test_attributes(self):
-        assert self.obj.name == "heating"
-        assert self.obj.label == "Heating"
-        assert self.obj.manufacturer == "Zodiac"
-        assert self.obj.model == "Thermostat"
-        assert self.obj.state == "20"
-        assert self.obj.is_on is True
-        assert self.obj.min_temperature == 1
-        assert self.obj.max_temperature == 40
+        water_temp = {
+            "name": "sns_3",
+            "sensor_type": "Water Temp",
+            "state": 1,
+            "value": 16,
+        }
+        self.water_temp = ExoDevice.from_data(self.system, water_temp)
 
-    async def test_turn_on(self):
-        self.system.set_heating.reset_mock()
-        self.obj.data["enabled"] = 0
-        await self.obj.turn_on()
-        self.system.set_heating.assert_called_with("enabled", 1)
+        devices = [
+            self.pool_set_point,
+            self.water_temp,
+        ]
+        self.system.devices = {x.name: x for x in devices}
 
-    async def test_turn_on_noop(self):
-        self.system.set_heating.reset_mock()
-        self.obj.data["enabled"] = 1
-        await self.obj.turn_on()
-        self.system.set_heating.assert_not_called()
+        self.sut = self.pool_set_point
+        self.sut_class = ExoThermostat
 
-    async def test_turn_off(self):
-        self.system.set_heating.reset_mock()
-        self.obj.data["enabled"] = 1
-        await self.obj.turn_off()
-        self.system.set_heating.assert_called_with("enabled", 0)
+    def test_property_label(self) -> None:
+        assert self.sut.label == "Heating"
 
-    async def test_turn_off_noop(self):
-        self.system.set_heating.reset_mock()
-        self.obj.data["enabled"] = 0
-        await self.obj.turn_off()
-        self.system.set_heating.assert_not_called()
+    def test_property_name(self) -> None:
+        assert self.sut.name == "heating"
 
-    async def test_bad_temperature(self):
-        with pytest.raises(Exception):
-            await self.obj.set_temperature(42)
+    def test_property_state(self) -> None:
+        assert self.sut.state == "1"
 
-    async def test_set_temperature(self):
-        self.obj.system.set_heating.reset_mock()
-        await self.obj.set_temperature(20)
-        self.obj.system.set_heating.assert_called_with("sp", 20)
+    def test_property_is_on_true(self) -> None:
+        self.sut.data["enabled"] = 1
+        super().test_property_is_on_true()
+
+    def test_property_is_on_false(self) -> None:
+        self.sut.data["enabled"] = 0
+        super().test_property_is_on_false()
+
+    def test_property_unit(self) -> None:
+        assert self.sut.unit == "C"
+
+    @pytest.mark.skip
+    def test_property_min_temperature_f(self) -> None:
+        pass
+
+    def test_property_min_temperature_c(self) -> None:
+        self.sut.system.temp_unit = "C"
+        super().test_property_min_temperature_c()
+        assert self.sut.min_temperature == EXO_TEMP_CELSIUS_LOW
+
+    @pytest.mark.skip
+    def test_property_max_temperature_f(self) -> None:
+        pass
+
+    def test_property_max_temperature_c(self) -> None:
+        self.sut.system.temp_unit = "C"
+        super().test_property_max_temperature_c()
+        assert self.sut.max_temperature == EXO_TEMP_CELSIUS_HIGH
+
+    def test_property_current_temperature(self) -> None:
+        super().test_property_current_temperature()
+        assert self.sut.current_temperature == "16"
+
+    def test_property_target_temperature(self) -> None:
+        super().test_property_target_temperature()
+        assert self.sut.target_temperature == "20"
+
+    async def test_turn_on(self) -> None:
+        self.sut.data["enabled"] = 0
+        await super().test_turn_on()
+        assert len(self.respx_calls) == 1
+        content = self.respx_calls[0].request.content.decode("utf-8")
+        assert "heating" in content
+
+    async def test_turn_on_noop(self) -> None:
+        self.sut.data["enabled"] = 1
+        await super().test_turn_on_noop()
+
+    async def test_turn_off(self) -> None:
+        self.sut.data["enabled"] = 1
+        await super().test_turn_off()
+        assert len(self.respx_calls) == 1
+        content = self.respx_calls[0].request.content.decode("utf-8")
+        assert "heating" in content
+
+    async def test_turn_off_noop(self) -> None:
+        self.sut.data["enabled"] = 0
+        await super().test_turn_off_noop()
+
+    @pytest.mark.skip
+    async def test_set_temperature_86f(self) -> None:
+        pass
+
+    async def test_set_temperature_30c(self) -> None:
+        await super().test_set_temperature_30c()
+        assert len(self.respx_calls) == 1
+        content = self.respx_calls[0].request.content.decode("utf-8")
+        assert "heating" in content
