e1f1bb6bc76eaa51ca2d90a39fc63c55d66973e2
Jorgen Evens <jorgen@evens.eu>
2024-07-31 02:08:32 +0200

    feat(zs500): initial device support for heat pump

---
+++ src/iaqualink/systems/zs500/device.py
@@ -0,0 +1,197 @@
+from __future__ import annotations
+
+import logging
+from enum import Enum, unique
+from typing import TYPE_CHECKING, Dict, Optional, Type, cast
+
+from iaqualink.device import (
+    AqualinkBinarySensor,
+    AqualinkDevice,
+    AqualinkLight,
+    AqualinkSensor,
+    AqualinkSwitch,
+    AqualinkThermostat,
+)
+from iaqualink.exception import (
+    AqualinkDeviceNotSupported,
+    AqualinkInvalidParameterException,
+)
+from iaqualink.typing import DeviceData
+
+if TYPE_CHECKING:
+    from iaqualink.systems.iaqua.system import Zs500System
+
+ZS500_TEMP_SCALE = 0.1
+ZS500_TEMP_CELSIUS_LOW = 15
+ZS500_TEMP_CELSIUS_HIGH = 32
+
+LOGGER = logging.getLogger("iaqualink")
+
+@unique
+class AqualinkState(Enum):
+    OFF = "0"
+    ON = "1"
+    ENABLED = "3"
+    ABSENT = "absent"
+    PRESENT = "present"
+
+
+class Zs500Device(AqualinkDevice):
+    def __init__(self, system: Zs500System, data: DeviceData):
+        super().__init__(system, data)
+
+        # This silences mypy errors due to AqualinkDevice type annotations.
+        self.system: Zs500System = system
+
+    @property
+    def label(self) -> str:
+        if "label" in self.data:
+            label = self.data["label"]
+            return " ".join([x.capitalize() for x in label.split()])
+
+        label = self.name
+        return " ".join([x.capitalize() for x in label.split("_")])
+
+    @property
+    def state(self) -> str:
+        return self.data["value"]
+
+    @property
+    def name(self) -> str:
+        return self.data["type"]
+
+    @property
+    def manufacturer(self) -> str:
+        return "Zodiac"
+
+    @property
+    def model(self) -> str:
+        return self.__class__.__name__.replace("Zs500", "")
+
+    @classmethod
+    def from_data(cls, system: Zs500System, data: DeviceData) -> Zs500Device:
+        class_: Type[Zs500Device]
+
+        device_type = data["et"]
+        if device_type == "HEAT_PUMP":
+            class_ = Zs500Thermostat
+        else:
+            raise AqualinkDeviceNotSupported(data)
+
+        return class_(system, data)
+
+
+class Zs500Sensor(Zs500Device, AqualinkSensor):
+    pass
+
+class Zs500TemperatureSensor(Zs500Sensor):
+
+    @property
+    def state(self) -> str:
+        return round(self.data["value"] * ZS500_TEMP_SCALE, 1)
+
+
+class Zs500BinarySensor(Zs500Sensor, AqualinkBinarySensor):
+    """These are non-actionable sensors, essentially read-only on/off."""
+
+    @property
+    def is_on(self) -> bool:
+        return (
+            AqualinkState(self.state)
+            in [AqualinkState.ON, AqualinkState.ENABLED, AqualinkState.PRESENT]
+            if self.state
+            else False
+        )
+
+
+class Zs500Switch(Zs500BinarySensor, AqualinkSwitch):
+    async def _toggle(self) -> None:
+        await self.system.set_switch(f"set_{self.name}")
+
+    async def turn_on(self) -> None:
+        if not self.is_on:
+            await self._toggle()
+
+    async def turn_off(self) -> None:
+        if self.is_on:
+            await self._toggle()
+
+class Zs500Thermostat(Zs500Switch, AqualinkThermostat):
+    @property
+    def _type(self) -> str:
+        return self.data["dt"]
+
+    @property
+    def unit(self) -> str:
+        return self.system.temp_unit
+
+    def _sensor(self, sensor_type: str) -> Zs500TemperatureSensor:
+        for name, sensor in self.data.items():
+            if not name.startswith("sns_"):
+                continue
+            if sensor["type"] != sensor_type:
+                continue
+            return Zs500TemperatureSensor(self.system, sensor)
+
+    @property
+    def _air_sensor(self) -> Zs500TemperatureSensor:
+        return self._sensor("air")
+
+    @property
+    def _water_sensor(self) -> Zs500TemperatureSensor:
+        return self._sensor("water")
+
+    @property
+    def air_temperature(self) -> str:
+        return self._air_sensor.state
+
+    @property
+    def current_temperature(self) -> str:
+        return self._water_sensor.state
+
+    @property
+    def target_temperature(self) -> str:
+        return round(self.data["tsp"] * ZS500_TEMP_SCALE, 1)
+
+    @property
+    def min_temperature(self) -> int:
+        return ZS500_TEMP_CELSIUS_LOW
+
+    @property
+    def max_temperature(self) -> int:
+        return ZS500_TEMP_CELSIUS_HIGH
+
+    async def set_temperature(self, temperature: int) -> None:
+        unit = self.unit
+        low = self.min_temperature
+        high = self.max_temperature
+
+        if temperature > high or temperature < low:
+            msg = f"{temperature}{unit} isn't a valid temperature"
+            msg += f" ({low}-{high}{unit})."
+            raise AqualinkInvalidParameterException(msg)
+
+        value = round(temperature * 10)
+        await self.system.set_device_property(self, value, "tsp")
+
+    @property
+    def is_cooling_mode_on(self) -> bool:
+        return self.data["cl"] > 0
+
+    async def turn_on_cooling_mode(self) -> None:
+        await self.system.set_device_property(self, 1, "cl")
+
+    async def turn_off_cooling_mode(self) -> None:
+        await self.system.set_device_property(self, 0, "cl")
+
+    @property
+    def is_on(self) -> bool:
+        return self.data["state"] > 0
+
+    async def turn_on(self) -> None:
+        if self.is_on is False:
+            await self.system.set_device_property(self, 1, "state")
+
+    async def turn_off(self) -> None:
+        if self.is_on is True:
+            await self.system.set_device_property(self, 0, "state")
---
+++ src/iaqualink/systems/zs500/system.py
@@ -0,0 +1,234 @@
+from __future__ import annotations
+
+import logging
+import time
+import asyncio
+from base64 import b64decode
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
+from typing import TYPE_CHECKING, Optional
+from awscrt import mqtt5, auth
+from awsiot import iotshadow, mqtt5_client_builder
+from uuid import uuid4
+
+import httpx
+
+from iaqualink.const import (
+    MIN_SECS_TO_REFRESH,
+    AQUALINK_AWS_IOT_ENDPOINT,
+    AQUALINK_AWS_IOT_REGION,
+)
+from iaqualink.exception import (
+    AqualinkDeviceNotSupported,
+    AqualinkServiceException,
+    AqualinkSystemOfflineException,
+)
+from iaqualink.system import AqualinkSystem
+from iaqualink.systems.zs500.device import Zs500Device
+from iaqualink.typing import Payload
+
+if TYPE_CHECKING:
+    from iaqualink.client import AqualinkClient
+
+LOGGER = logging.getLogger("iaqualink")
+
+def create_callback(coro):
+    loop = asyncio.get_running_loop()
+
+    def callback(data):
+        asyncio.run_coroutine_threadsafe(coro(data), loop)
+
+    return callback
+
+class Zs500System(AqualinkSystem):
+    NAME = "zs500"
+
+    def __init__(self, aqualink: AqualinkClient, data: Payload):
+        super().__init__(aqualink, data)
+
+        self._updates = {}
+        self._started = False
+        self.temp_unit = 'C'
+
+        self._shadow = None
+
+    def __repr__(self) -> str:
+        attrs = ["name", "serial", "data"]
+        attrs = ["%s=%r" % (i, getattr(self, i)) for i in attrs]
+        return f'{self.__class__.__name__}({" ".join(attrs)})'
+
+    async def _connect(self) -> None:
+        credentials = auth.AwsCredentialsProvider.new_static(
+            self.aqualink._iot_credentials["AccessKeyId"],
+            self.aqualink._iot_credentials["SecretKey"],
+            self.aqualink._iot_credentials["SessionToken"],
+        )
+
+        connected = asyncio.Future()
+        async def on_connected(data):
+            connected.set_result(True)
+
+        async def on_failure(data):
+            connected.set_exception(data)
+
+        async def on_stopped(data):
+            self._started = False
+
+        mqtt = mqtt5_client_builder.websockets_with_default_aws_signing(
+            region=AQUALINK_AWS_IOT_REGION,
+            endpoint=AQUALINK_AWS_IOT_ENDPOINT,
+            credentials_provider=credentials,
+            on_lifecycle_stopped=create_callback(on_stopped),
+            on_lifecycle_connection_success=create_callback(on_connected),
+            on_lifecycle_connection_failure=create_callback(on_failure),
+        )
+        mqtt.start()
+
+        await asyncio.wait_for(connected, timeout=5)
+
+        self._shadow = iotshadow.IotShadowClient(mqtt)
+        await self._attach_shadow_handlers()
+
+        self._started = True
+
+    async def _attach_shadow_handlers(self) -> None:
+
+        async def on_reject(response: Any):
+            if response.client_token in self._updates:
+                self._updates[response.client_token].set_exception(response)
+
+        async def on_accept(response: Any):
+            if response.client_token in self._updates:
+                self._updates[response.client_token].set_result(response)
+
+        # Get Shadow
+        ##########################
+        accept_future, _ = self._shadow.subscribe_to_get_shadow_accepted(
+            request=iotshadow.GetShadowSubscriptionRequest(thing_name=self.serial),
+            qos=mqtt5.QoS.AT_LEAST_ONCE,
+            callback=create_callback(on_accept))
+
+        reject_future, _ = self._shadow.subscribe_to_get_shadow_rejected(
+            request=iotshadow.GetShadowSubscriptionRequest(thing_name=self.serial),
+            qos=mqtt5.QoS.AT_LEAST_ONCE,
+            callback=create_callback(on_reject))
+
+        # On shadow updates
+        ##########################
+        async def on_shadow_updated(response: iotshadow.ShadowUpdatedEvent):
+            await self._parse_device_info(response.current)
+
+        update_future, _ = self._shadow.subscribe_to_shadow_updated_events(
+            request=iotshadow.ShadowUpdatedSubscriptionRequest(thing_name=self.serial),
+            qos=mqtt5.QoS.AT_LEAST_ONCE,
+            callback=create_callback(on_shadow_updated),
+        )
+
+        # Update Shadow
+        ##########################
+        update_accept_future, _ = self._shadow.subscribe_to_update_shadow_accepted(
+            request=iotshadow.UpdateShadowSubscriptionRequest(thing_name=self.serial),
+            qos=mqtt5.QoS.AT_LEAST_ONCE,
+            callback=create_callback(on_accept),
+        )
+
+        reject_accept_future, _ = self._shadow.subscribe_to_update_shadow_rejected(
+            request=iotshadow.UpdateShadowSubscriptionRequest(thing_name=self.serial),
+            qos=mqtt5.QoS.AT_LEAST_ONCE,
+            callback=create_callback(on_reject),
+        )
+
+        await asyncio.gather(
+            asyncio.wrap_future(accept_future),
+            asyncio.wrap_future(reject_future),
+
+            asyncio.wrap_future(update_future),
+
+            asyncio.wrap_future(update_accept_future),
+            asyncio.wrap_future(reject_accept_future),
+        )
+
+    async def set_device_property(self, device: Zs500Device, value: Any, *path) -> None:
+        for k, dev in self.devices.items():
+            if dev == device:
+                await self.set_shadow_single(value, "equipment", k, *path)
+                return
+        raise AqualinkDeviceNotSupported()
+
+    async def set_shadow_single(self, value: Any, *keys) -> None:
+        desired = {}
+
+        target = desired
+        for key in keys[:-1]:
+            target = target.setdefault(key, {})
+        target[keys[-1]] = value
+
+        await self.set_shadow(desired)
+
+    async def set_shadow(self, desired: dict[str, dict]) -> None:
+        token = str(uuid4)
+        ready = asyncio.Future()
+        self._updates[token] = ready
+
+        publish = self._shadow.publish_update_shadow(
+            request=iotshadow.UpdateShadowRequest(
+                client_token=token,
+                state=iotshadow.ShadowState(desired=desired),
+                thing_name=self.serial,
+            ),
+            qos=mqtt5.QoS.AT_LEAST_ONCE,
+        )
+
+        try:
+            await asyncio.wrap_future(publish)
+            await asyncio.wait_for(ready, timeout=10)
+        finally:
+            del self._updates[token]
+
+
+    async def _parse_device_info(self, data: Any) -> None:
+        report = data.state.reported
+        equipment = report["equipment"]
+
+        if report["aws"]["status"] != "connected":
+            LOGGER.warning(f"Status for system {self.serial} is Offline.")
+            raise AqualinkSystemOfflineException
+
+        for k, v in equipment.items():
+            if k in self.devices:
+                self.devices[k].data = v
+            else:
+                self.devices[k] = Zs500Device.from_data(self, v)
+
+    async def update(self) -> None:
+        now = int(time.time())
+        delta = now - self.last_refresh
+        if delta < MIN_SECS_TO_REFRESH:
+            LOGGER.debug(f"Only {delta}s since last refresh.")
+            return
+
+        if not self._started:
+            await self._connect()
+
+        token = str(uuid4())
+        ready = asyncio.Future()
+
+        self._updates[token] = ready
+
+        get_request = self._shadow.publish_get_shadow(
+            request=iotshadow.GetShadowRequest(
+                thing_name=self.serial,
+                client_token=token
+            ),
+            qos=mqtt5.QoS.AT_LEAST_ONCE,
+        )
+
+        try:
+            await asyncio.wrap_future(get_request)
+            response = await asyncio.wait_for(ready, timeout=10)
+            await self._parse_device_info(response)
+        finally:
+            del self._updates[token]
+
+        self.online = True
+        self.last_refresh = int(time.time())
