bca8cc7081e1b52d013de0be1a16171140db25a4
Florent Thoumie <florent@thoumie.net>
2025-03-17 12:49:15 -0700

    Import i2d code from tkhduracell

---
+++ src/iaqualink/systems/i2d/system.py
@@ -0,0 +1,56 @@
+import httpx
+import json
+from typing import Optional, TYPE_CHECKING
+
+from iaqualink.system import AqualinkSystem
+from iaqualink.const import (
+    AQUALINK_API_KEY,
+)
+from iaqualink.typing import Payload
+
+from .device import AquaLinkIQPump
+
+
+if TYPE_CHECKING:
+    from iaqualink.client import AqualinkClient
+
+AQUALINK_HTTP_HEADERS = {
+    "user-agent": "okhttp/3.14.7",
+    "content-type": "application/json",
+}
+IAQUA_DEVICE_URL = "https://r-api.iaqualink.net/v2/devices/"
+
+
+class I2DSystem(AqualinkSystem):
+    NAME = "i2d"
+
+    def __init__(self, aqualink: "AqualinkClient", data: Payload):
+        super().__init__(aqualink, data)
+
+    def __repr__(self) -> str:
+        attrs = ["name", "serial", "data"]
+        attrs = ["%s=%r" % (i, getattr(self, i)) for i in attrs]
+        return f"{self.__class__.__name__}({' '.join(attrs)})"
+
+    async def update(self) -> None:
+        resp = await self._send_device_request()
+        data: dict = resp.json()
+        self.devices = {self.serial: AquaLinkIQPump(self, data["alldata"])}
+
+    async def _send_device_request(
+        self,
+        params: Optional[Payload] = None,
+    ) -> httpx.Response:
+        if not params:
+            params = {}
+
+        r = await self.aqualink._send_login_request()
+        id_token = r.json()["userPoolOAuth"]["IdToken"]
+
+        url = f"{IAQUA_DEVICE_URL}{self.serial}/control.json"
+        headers = {"api_key": AQUALINK_API_KEY, "Authorization": id_token}
+        headers.update(**AQUALINK_HTTP_HEADERS)
+        data = json.dumps(
+            {"user_id": self.aqualink._user_id, "command": "/alldata/read"}
+        )
+        return await self.aqualink._client.post(url, headers=headers, data=data)
