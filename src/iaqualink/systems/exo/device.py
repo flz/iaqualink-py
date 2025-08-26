53bd317a95c5b7fb7b21d57ca1c18549956cfb07
Florent Thoumie <florent@thoumie.net>
2025-08-25 18:16:20 -0700

    feat(exo): add support for eXO systems (#21)
    Co-authored-by: ozrex75 <ozrex75@gmail.com>
    Co-authored-by: Martin French <martin.frenchy@gmail.com>

---
+++ src/iaqualink/systems/exo/device.py
@@ -0,0 +1,203 @@
+from __future__ import annotations
+
+import logging
+from enum import Enum, unique
+from typing import TYPE_CHECKING, Any, cast
+
+from iaqualink.device import (
+    AqualinkDevice,
+    AqualinkSensor,
+    AqualinkSwitch,
+    AqualinkThermostat,
+)
+from iaqualink.exception import AqualinkInvalidParameterException
+
+if TYPE_CHECKING:
+    from collections.abc import Callable, Coroutine
+
+    from iaqualink.systems.exo.system import ExoSystem
+    from iaqualink.typing import DeviceData
+
+EXO_TEMP_CELSIUS_LOW = 1
+EXO_TEMP_CELSIUS_HIGH = 40
+
+LOGGER = logging.getLogger("iaqualink")
+
+
+@unique
+class ExoState(Enum):
+    OFF = 0
+    ON = 1
+
+
+class ExoDevice(AqualinkDevice):
+    def __init__(self, system: ExoSystem, data: DeviceData):
+        super().__init__(system, data)
+
+        # This silences mypy errors due to AqualinkDevice type annotations.
+        self.system: ExoSystem = system
+
+    @property
+    def label(self) -> str:
+        name = self.name
+        return " ".join([x.capitalize() for x in name.split("_")])
+
+    @property
+    def state(self) -> str:
+        return str(self.data["state"])
+
+    @property
+    def name(self) -> str:
+        return self.data["name"]
+
+    @property
+    def manufacturer(self) -> str:
+        return "Zodiac"
+
+    @property
+    def model(self) -> str:
+        return self.__class__.__name__.replace("Exo", "")
+
+    @classmethod
+    def from_data(cls, system: ExoSystem, data: DeviceData) -> ExoDevice:
+        class_: type[ExoDevice]
+
+        if data["name"].startswith("aux_"):
+            class_ = ExoAuxSwitch
+        elif data["name"].startswith("sns_"):
+            class_ = ExoSensor
+        elif data["name"] == "heating":
+            class_ = ExoThermostat
+        elif data["name"] == "heater":
+            class_ = ExoHeater
+        elif data["name"] in ["production", "boost", "low"]:
+            class_ = ExoAttributeSwitch
+        else:
+            class_ = ExoAttributeSensor
+
+        return class_(system, data)
+
+
+class ExoSensor(ExoDevice, AqualinkSensor):
+    """These sensors are called sns_#."""
+
+    @property
+    def is_on(self) -> bool:
+        return ExoState(self.data["state"]) == ExoState.ON
+
+    @property
+    def state(self) -> str:
+        if self.is_on:
+            return str(self.data["value"])
+        return ""
+
+    @property
+    def label(self) -> str:
+        return self.data["sensor_type"]
+
+    @property
+    def name(self) -> str:
+        # XXX: We're using the label as name rather than "sns_#".
+        # Might revisit later.
+        return self.data["sensor_type"].lower().replace(" ", "_")
+
+
+class ExoAttributeSensor(ExoDevice, AqualinkSensor):
+    """These sensors are a simple key/value in equipment->swc_0."""
+
+
+# This is an abstract class, not to be instantiated directly.
+class ExoSwitch(ExoDevice, AqualinkSwitch):
+    @property
+    def label(self) -> str:
+        return self.name.replace("_", " ").capitalize()
+
+    @property
+    def is_on(self) -> bool:
+        return ExoState(self.data["state"]) == ExoState.ON
+
+    @property
+    def _command(self) -> Callable[[str, int], Coroutine[Any, Any, None]]:
+        raise NotImplementedError
+
+    async def turn_on(self) -> None:
+        if not self.is_on:
+            await self._command(self.name, 1)
+
+    async def turn_off(self) -> None:
+        if self.is_on:
+            await self._command(self.name, 0)
+
+
+class ExoAuxSwitch(ExoSwitch):
+    @property
+    def _command(self) -> Callable[[str, int], Coroutine[Any, Any, None]]:
+        return self.system.set_aux
+
+
+class ExoAttributeSwitch(ExoSwitch):
+    @property
+    def _command(self) -> Callable[[str, int], Coroutine[Any, Any, None]]:
+        return self.system.set_toggle
+
+
+class ExoHeater(ExoDevice):
+    """This device is to seperate the state of the heater from the thermostat to maintain the existing homeassistant API"""
+
+
+class ExoThermostat(ExoSwitch, AqualinkThermostat):
+    @property
+    def state(self) -> str:
+        return str(self.data["sp"])
+
+    @property
+    def unit(self) -> str:
+        return "C"
+
+    @property
+    def _sensor(self) -> ExoSensor:
+        return cast(ExoSensor, self.system.devices["sns_3"])
+
+    @property
+    def _heater(self) -> ExoHeater:
+        return cast(ExoSensor, self.system.devices["heater"])
+
+    @property
+    def current_temperature(self) -> str:
+        return self._sensor.state
+
+    @property
+    def target_temperature(self) -> str:
+        return str(self.data["sp"])
+
+    @property
+    def min_temperature(self) -> int:
+        return int(self.data["sp_min"])
+
+    @property
+    def max_temperature(self) -> int:
+        return int(self.data["sp_max"])
+
+    async def set_temperature(self, temperature: int) -> None:
+        unit = self.unit
+        low = self.min_temperature
+        high = self.max_temperature
+
+        if temperature not in range(low, high + 1):
+            msg = f"{temperature}{unit} isn't a valid temperature"
+            msg += f" ({low}-{high}{unit})."
+            raise AqualinkInvalidParameterException(msg)
+
+        await self.system.set_heating("sp", temperature)
+
+    @property
+    def is_on(self) -> bool:
+        return ExoState(self.data["enabled"]) == ExoState.ON
+
+    async def turn_on(self) -> None:
+        if self.is_on is False:
+            await self.system.set_heating("enabled", 1)
+
+    async def turn_off(self) -> None:
+        if self.is_on is True:
+            await self.system.set_heating("enabled", 0)
---
+++ src/iaqualink/systems/exo/system.py
@@ -0,0 +1,151 @@
+from __future__ import annotations
+
+import logging
+import time
+from typing import TYPE_CHECKING, Any
+
+from iaqualink.const import MIN_SECS_TO_REFRESH
+from iaqualink.exception import (
+    AqualinkServiceException,
+    AqualinkServiceUnauthorizedException,
+    AqualinkSystemOfflineException,
+)
+from iaqualink.system import AqualinkSystem
+from iaqualink.systems.exo.device import ExoDevice
+
+if TYPE_CHECKING:
+    import httpx
+
+    from iaqualink.client import AqualinkClient
+    from iaqualink.typing import Payload
+
+EXO_DEVICES_URL = "https://prod.zodiac-io.com/devices/v1"
+
+
+LOGGER = logging.getLogger("iaqualink")
+
+
+class ExoSystem(AqualinkSystem):
+    NAME = "exo"
+
+    def __init__(self, aqualink: AqualinkClient, data: Payload):
+        super().__init__(aqualink, data)
+        # This lives in the parent class but mypy complains.
+        self.last_refresh: int = 0
+        self.temp_unit = "C"  # TODO: check if unit can be changed on panel?
+
+    def __repr__(self) -> str:
+        attrs = ["name", "serial", "data"]
+        attrs = [f"{i}={getattr(self, i)!r}" for i in attrs]
+        return f"{self.__class__.__name__}({' '.join(attrs)})"
+
+    async def send_devices_request(self, **kwargs: Any) -> httpx.Response:
+        url = f"{EXO_DEVICES_URL}/{self.serial}/shadow"
+        headers = {"Authorization": self.aqualink.id_token}
+
+        try:
+            r = await self.aqualink.send_request(url, headers=headers, **kwargs)
+        except AqualinkServiceUnauthorizedException:
+            # token expired so refresh the token and try again
+            await self.aqualink.login()
+            headers = {"Authorization": self.aqualink.id_token}
+            r = await self.aqualink.send_request(url, headers=headers, **kwargs)
+
+        return r
+
+    async def send_reported_state_request(self) -> httpx.Response:
+        return await self.send_devices_request()
+
+    async def send_desired_state_request(
+        self, state: dict[str, Any]
+    ) -> httpx.Response:
+        return await self.send_devices_request(
+            method="post", json={"state": {"desired": state}}
+        )
+
+    async def update(self) -> None:
+        # Be nice to Aqualink servers since we rely on polling.
+        now = int(time.time())
+        delta = now - self.last_refresh
+        if delta < MIN_SECS_TO_REFRESH:
+            LOGGER.debug(f"Only {delta}s since last refresh.")
+            return
+
+        try:
+            r = await self.send_reported_state_request()
+        except AqualinkServiceException:
+            self.online = None
+            raise
+
+        try:
+            self._parse_shadow_response(r)
+        except AqualinkSystemOfflineException:
+            self.online = False
+            raise
+
+        self.online = True
+        self.last_refresh = int(time.time())
+
+    def _parse_shadow_response(self, response: httpx.Response) -> None:
+        data = response.json()
+
+        LOGGER.debug(f"Shadow response: {data}")
+
+        devices = {}
+
+        # Process the chlorinator attributes[equipmen]
+        # Make the data a bit flatter.
+        root = data["state"]["reported"]["equipment"]["swc_0"]
+        for name, state in root.items():
+            attrs = {"name": name}
+            if isinstance(state, dict):
+                attrs.update(state)
+            else:
+                attrs.update({"state": state})
+            devices.update({name: attrs})
+
+        # Remove those values, they're not handled properly.
+        devices.pop("boost_time", None)
+        devices.pop("vsp_speed", None)
+        devices.pop("sn", None)
+        devices.pop("vr", None)
+        devices.pop("version", None)
+
+        # Process the heating control attributes
+        if "heating" in data["state"]["reported"]:
+            name = "heating"
+            attrs = {"name": name}
+            attrs.update(data["state"]["reported"]["heating"])
+            devices.update({name: attrs})
+            # extract heater state into seperate device to maintain homeassistant API
+            name = "heater"
+            attrs = {"name": name}
+            attrs.update(
+                {"state": data["state"]["reported"]["heating"]["state"]}
+            )
+            devices.update({name: attrs})
+
+        LOGGER.debug(f"devices: {devices}")
+
+        for k, v in devices.items():
+            if k in self.devices:
+                for dk, dv in v.items():
+                    self.devices[k].data[dk] = dv
+            else:
+                self.devices[k] = ExoDevice.from_data(self, v)
+
+    async def set_heating(self, name: str, state: int) -> None:
+        r = await self.send_desired_state_request({"heating": {name: state}})
+        r.raise_for_status()
+
+    async def set_aux(self, aux: str, state: int) -> None:
+        r = await self.send_desired_state_request(
+            {"equipment": {"swc_0": {aux: {"state": state}}}}
+        )
+        r.raise_for_status()
+
+    async def set_toggle(self, name: str, state: int) -> None:
+        r = await self.send_desired_state_request(
+            {"equipment": {"swc_0": {name: state}}}
+        )
+        r.raise_for_status()
